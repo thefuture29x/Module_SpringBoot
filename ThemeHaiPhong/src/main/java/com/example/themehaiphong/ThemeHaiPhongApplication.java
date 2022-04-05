53430a2dbb1189e65d775e47218bf1c97b7a339b
Hai Phong <teothiphong@email.com>
2022-04-05 23:51:49 +0700

    create folder ThemeHaiPhong

---
+++ ThemeHaiPhong/src/main/java/com/example/themehaiphong/ThemeHaiPhongApplication.java
@@ -0,0 +1,13 @@
+package com.example.themehaiphong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeHaiPhongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThemeHaiPhongApplication.class, args);
+    }
+
+}
---
+++ ThemeHaiPhong/src/test/java/com/example/themehaiphong/ThemeHaiPhongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.themehaiphong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeHaiPhongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
