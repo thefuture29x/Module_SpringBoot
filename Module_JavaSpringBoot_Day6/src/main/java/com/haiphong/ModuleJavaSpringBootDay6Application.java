b0a76ec2b54428ba94a0df0b7c62433a46af3922
Hai Phong <teothiphong@email.com>
2022-04-05 09:07:35 +0700

    create file in package entity and dao

---
+++ Module_JavaSpringBoot_Day6/src/main/java/com/haiphong/ModuleJavaSpringBootDay6Application.java
@@ -0,0 +1,9 @@
+package com.haiphong;
+
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class ModuleJavaSpringBootDay6Application {
+
+//    StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+}
---
+++ Module_JavaSpringBoot_Day6/src/main/java/com/haiphong/dao/StudentDAO.java
@@ -0,0 +1,9 @@
+package com.haiphong.dao;
+
+import org.hibernate.SessionFactory;
+
+public class StudentDAO {
+    // khoi tao doi tuong factory session
+    // call config connector
+    private static SessionFactory factory;
+}
