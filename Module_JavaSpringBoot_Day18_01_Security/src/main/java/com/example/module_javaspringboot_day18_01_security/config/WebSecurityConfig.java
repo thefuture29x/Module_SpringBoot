25983882bc032a35f18545cb86a298bb5dc52c2d
Hai Phong <teothiphong@email.com>
2022-04-22 16:54:35 +0700

    update feature Remember me when user login

--- Module_JavaSpringBoot_Day18_01_Security/src/main/java/com/example/module_javaspringboot_day18_01_security/config/WebSecurityConfig.java
+++ Module_JavaSpringBoot_Day18_01_Security/src/main/java/com/example/module_javaspringboot_day18_01_security/config/WebSecurityConfig.java
@@ -1,14 +1,25 @@
 package com.example.module_javaspringboot_day18_01_security.config;
 
+import com.example.module_javaspringboot_day18_01_security.entities.UserEntity;
+import com.example.module_javaspringboot_day18_01_security.repository.IUserRepository;
+import com.example.module_javaspringboot_day18_01_security.security.CustomerService;
 import com.example.module_javaspringboot_day18_01_security.security.LoginSuccessHandler;
 import com.example.module_javaspringboot_day18_01_security.security.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -16,6 +27,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     UserService userService;
     @Autowired
     LoginSuccessHandler loginSuccessHandler;
+    @Autowired
+    IUserRepository iUserRepository;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -26,9 +39,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
+    @EventListener(
+            classes = InteractiveAuthenticationSuccessEvent.class
+    )
+    public void authenticationSuccessEvent(InteractiveAuthenticationSuccessEvent event) {
+        Authentication auth = event.getAuthentication();
+        HttpSession session = ((ServletRequestAttributes) Objects.requireNonNull(
+                RequestContextHolder.currentRequestAttributes()))
+                .getRequest().getSession();
+
+        CustomerService userEntity = (CustomerService) auth.getPrincipal();
+        UserEntity user = iUserRepository.findByUsername(userEntity.getUsername());
+        session.setAttribute("user",user);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.rememberMe().key("uniqueAndSecret").tokenValiditySeconds(1296000);
         http.authorizeRequests()
 //                .antMatchers("/", "/home", "/add", "/register-user","/register").permitAll()
                 .antMatchers("/admin/**").hasAnyAuthority("ROLE_ADMIN")
@@ -48,11 +75,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .exceptionHandling()
-                .accessDeniedPage("/403")
-                .and()
-                .rememberMe()
-                .key("haiPhong@9")
-
-        ;
+                .accessDeniedPage("/403");
     }
 }
