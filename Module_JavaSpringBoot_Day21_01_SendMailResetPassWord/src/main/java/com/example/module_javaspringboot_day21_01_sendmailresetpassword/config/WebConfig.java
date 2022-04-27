58c96a73e96ccf89f08e9803ce67fdbb42cea98a
Hai Phong <teothiphong@email.com>
2022-04-27 14:38:04 +0700

    add theme for form login

--- Module_JavaSpringBoot_Day21_01_SendMailResetPassWord/src/main/java/com/example/module_javaspringboot_day21_01_sendmailresetpassword/config/WebConfig.java
+++ Module_JavaSpringBoot_Day21_01_SendMailResetPassWord/src/main/java/com/example/module_javaspringboot_day21_01_sendmailresetpassword/config/WebConfig.java
@@ -4,11 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("resources/static/css/");
+        Path pathDir = Paths.get("./src/main/resources/static");
+        String filePath = pathDir.toFile().getAbsolutePath();
+
+        registry.addResourceHandler("/css/**").addResourceLocations("file://" +filePath+"/css/");
     }
 }
